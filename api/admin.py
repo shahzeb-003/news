28ec685ab1b07e0f8fd8e8ce3bb913a166653cae
Shahzeb <99556355+shahzeb-003@users.noreply.github.com>
2023-11-21 17:58:56 +0000

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, News, Comment
 
 admin.site.register(CustomUser, UserAdmin)
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at', 'updated_at')
+    list_filter = ('category',)
+    search_fields = ('title',)  # Optional: enable searching by title
+
+admin.site.register(Comment)
